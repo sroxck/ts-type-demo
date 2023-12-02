61cb481f03bf215192d43b1afd0627101d1fb0e9
sroxck <642261686@qq.com>
2023-12-02 11:23:16 +0800

    feat: infer

--- infer.ts
+++ infer.ts
@@ -1,22 +1,26 @@
-// 实现一个可选工具参数 作用是将一个对象类型中的部分参数变成可选参数
-// 1. 首先需要将 可选的部分剔除 拿到 必填的部分 使用Omit 得到 title,conten
-// 2. 使用pick挑选出 需要变成可选的参数 data和index,使用partial 变成可选 
-// 3. 合并结果 Omit结果是{title:string,conteng:string} Partial:{data?:Date,index?:number} 使用 &合并为 {title:string,conteng:string,data?:Date,index?:number}
+/**
+ * infer 类型推断
+ * demo1: 推断出函数的返回值是什么类型,写一个类型工具 用infer推断函数返回值类型
+ */
 
-type article = {
-  title: string,
-  content: string,
-  data: Date,
-  index: number
-}
-type articleOpt = operator<article, 'data' | 'index'>
-type articleAny = articleOpt & {[key:string]:any}
-type operator<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
-function createArticle(obj: articleAny) {
-  return obj
-}
-createArticle({
-  title: 'string',
-  content: 'string',
-  任意属性:'都是可以的,因为使用了 articleAny 添加了索引签名'
-})
+type sum = (a: number, b: number) => number
+type Return<T> = T extends (...args: any[]) => infer R ? R : T
+let sumResult: Return<sum>;
+
+
+// demo2 取出promise泛型中的类型
+type PromiseType<T> = T extends Promise<infer K> ? K : T
+// 递归
+type PromiseTypeD<T> = T extends Promise<infer K> ? PromiseTypeD<K> : T
+type pt = PromiseType<Promise<string>>
+
+// demo3 返回函数的第一个参数的类型
+type FirstArg<T> = T extends (first:infer K,...args:any[])=>any ? K :T 
+type fa = FirstArg<(name:string,age:number)=>void>
+
+// demo4 获取数组每一项的类型
+type ArrayType<T> = T extends (infer I)[] ? I : T
+const list = [1,2,324,23,5,'sf']
+type ItemType = ArrayType<typeof list>
+
+
---
+++ optional.ts
@@ -0,0 +1,29 @@
+/*
+ * @Author: sroxck
+ * @Date: 2023-12-02 09:48:47
+ * @LastEditors: sroxck
+ * @LastEditTime: 2023-12-02 10:59:49
+ * @Description: 
+ */
+// 实现一个可选工具参数 作用是将一个对象类型中的部分参数变成可选参数
+// 1. 首先需要将 可选的部分剔除 拿到 必填的部分 使用Omit 得到 title,conten
+// 2. 使用pick挑选出 需要变成可选的参数 data和index,使用partial 变成可选 
+// 3. 合并结果 Omit结果是{title:string,conteng:string} Partial:{data?:Date,index?:number} 使用 &合并为 {title:string,conteng:string,data?:Date,index?:number}
+
+type article = {
+  title: string,
+  content: string,
+  data: Date,
+  index: number
+}
+type articleOpt = optional<article, 'data' | 'index'>
+type articleAny = articleOpt & {[key:string]:any}
+type optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+function createArticle(obj: articleAny) {
+  return obj
+}
+createArticle({
+  title: 'string',
+  content: 'string',
+  任意属性:'都是可以的,因为使用了 articleAny 添加了索引签名'
+})
